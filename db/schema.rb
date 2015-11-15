7a24d92d5cb1a099ff05bd0ebadbea70be8a9f82
littlestarling <takto78@gmail.com>
2015-11-15 19:40:57 +0900

    Handle microposts

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20151115081741) do
     t.datetime "updated_at", null: false
   end
 
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
